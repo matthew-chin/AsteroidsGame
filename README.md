68ae295eb0a1dc6841a0e5b448450a221f342ad5
unknown <machin@697-334-Thorin.sf.k12.internal>
2014-11-04 11:14:35 -0800

    first push

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Asteroids (Part 1)
+0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 Asteroids (Part 1)
 ==================
 In this assignment we will start to replicate the old video game Asteroids. You will write a program that has a space ship that can be controlled with the keyboard. You will need to write a `SpaceShip` class. Your `SpaceShip` class will extend the `Floater` class, an abstract class that represents all things that float in space. 
 
--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 int starlen = (int)(Math.random()*1000);
 
 SpaceShip vroom = new SpaceShip();
@@ -21,7 +37,7 @@ noStroke();
 public void draw() 
 {
   background(0);
-  if(wPressed){vroom.accelerate(0.15);}
+  if(wPressed){vroom.accelerate(0.15f);}
   if(aPressed){vroom.rotate(-5);}
   if(dPressed){vroom.rotate(5);}
   vroom.move();
@@ -86,7 +102,7 @@ class Star
   }
 }
 
-void keyReleased()
+public void keyReleased()
 {
   if(key == 'w')
   {
@@ -101,7 +117,7 @@ void keyReleased()
     aPressed = false;
   }
 }
-void keyPressed()
+public void keyPressed()
 {
   if(key == 'w')
   {
@@ -207,3 +223,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
